62de5b21c6efb6c11e9d54a2d04a5d3abc8bcd89
sobir-swe <sobirjon.swe@gmail.com>
2024-12-05 15:16:14 +0500

    feat: add migration for groups table

---
+++ database/migrations/2024_12_05_055237_create_groups_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('branch_id')->constrained('branches')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('stage_id')->constrained('stages')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('lesson_continuous')->nullable();
+            $table->string('weekday')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('groups');
+    }
+};
