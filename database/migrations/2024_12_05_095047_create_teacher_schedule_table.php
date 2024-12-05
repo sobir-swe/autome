fafe01340cd2b1e73dd374576b059fe5b1484940
sobir-swe <sobirjon.swe@gmail.com>
2024-12-05 15:16:37 +0500

    feat: add migration for teacher schedule table

---
+++ database/migrations/2024_12_05_095047_create_teacher_schedule_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher_schedule', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lesson_id')->constrained('lessons')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('instructor_id')->constrained('instructors')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->datetime('start_time');
+            $table->datetime('end_time');
+            $table->enum('status', ['planned', 'completed', 'cancelled'])->default('planned');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher_schedule');
+    }
+};
