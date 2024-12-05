0e60a282aa2dd160b71ec2bb7bd9174edf5dc842
sobir-swe <sobirjon.swe@gmail.com>
2024-12-05 15:16:02 +0500

    feat: add migration for students table

---
+++ database/migrations/2024_12_05_054159_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->foreignId('branch_id')->constrained('branches')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->foreignId('student_responsible_id')->constrained('student_responsibles')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('phone_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
